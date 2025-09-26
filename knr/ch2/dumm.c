1b9c1df303e8e6076eaad1930fe1ac2172d59727
charlsmaina <mainacharles1496@gmail.com>
2025-09-26 06:18:52 +0300

    testing max value of signed int

--- knr/ch2/dumm.c
+++ knr/ch2/dumm.c
@@ -3,5 +3,13 @@ int main()
 {
     printf("Hello, \rWorld\be!\n");
     printf("\a");
+
+    //size of int
+    printf("Int size: %zu bytes\n", sizeof(int));
+    printf("Max size of int: %d\n", __INT_MAX__);
+    int x = __INT_MAX__ + 1;
+    printf("Overflowed int: %d\n", x);
+   
+    
     return 0;
 }
