4ea2f498bbb954e490b1d1b72bf737cd8e9bc442
charlsmaina <mainacharles1496@gmail.com>
2025-10-12 17:26:24 +0300

    knr version

---
+++ knr/ch1/kwcount.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+main()
+{
+    int state, nl, nw, nc, c;
+    nl = nw = nc = 0;
+
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+
+        if (c == '\n' || c == ' ' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("Characters:%d\nWords:%d\nLines:%d\n", nc, nw, nl);
+}
