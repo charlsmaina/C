48d2088f740e6d8bf131caf447ee393121c14a41
charlsmaina <mainacharles1496@gmail.com>
2025-10-12 15:38:53 +0300

    words,lines,characters count

---
+++ knr/ch1/wcount.c
@@ -0,0 +1,8 @@
+// This is a program to count the number of wors, characters and lines from an inut stream
+
+#include <stdio.h>
+int main()
+{
+    int c, lines, words, chars;
+    lines = words = chars
+}
