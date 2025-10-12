04f159fd52c0d5b80779cc5d94aab152c3ada3bb
charlsmaina <mainacharles1496@gmail.com>
2025-10-12 15:35:58 +0300

    comments

--- knr/ch1/ex1-10.c
+++ knr/ch1/ex1-10.c
@@ -3,23 +3,29 @@ backspace by \b, and each backslash by \\. This makes tabs and backspaces visibl
 unambiguous way. */
 
 #include <stdio.h>
-int main ()
+int main()
 {
-    int c;
-    while ((c = getchar()) != EOF)
-    {
-    if (c == '\t')
-            printf("\\t");
-    if (c == '\b')
-            printf("\\b");
-    if (c == '\\')
-            printf("\\\\");
-    else
-    putchar(c);
-        
-    
-    }
-   
-    return 0;
-    
+        int c;
+        while ((c = getchar()) != EOF)
+        {
+                if (c == '\t')
+                        printf("\\t");
+                if (c == ' ')
+                        printf("\\b");
+                if (c == '\\')
+                        printf("\\\\");
+                if (c != '\t' && c != ' ' && c != '\\')
+                {
+                        putchar(c);
+                }
+        }
+
+        return 0;
 }
+/*
+  in a series of if's , the else is always associated with the previous if
+  But in an else if series , the else is associated with the whole body of if,else if
+  In this program using else if instead of if makes a difference
+  To use if only:
+   you will have to check if the previous characters are not either tab,blank or backslash.
+*/
--- knr/ch1/ex1-9V3.c
+++ knr/ch1/ex1-9V3.c
@@ -2,19 +2,28 @@
 more blanks by a single blank.*/
 
 #include <stdio.h>
-#define NONBLANK 'a'
+#define NONBLANK EOF
 
-int main ()
+int main()
 {
-int c,prevc;
-prevc = NONBLANK;
+  int c, prevc;
+  prevc = NONBLANK;
 
-while ((c = getchar()) != EOF)
-{
-  if (c != ' ' || prevc != ' ')
-  
-  putchar(c);
-  prevc = c ;
+  while ((c = getchar()) != EOF)
+  {
+    if (c != ' ' || prevc != ' ')
+
+      putchar(c);
+    prevc = c;
+  }
 }
 
-}
+/*
+ Here is another succint code;
+ Here the main concept id the operation of the || {OR operation}. In an OR operation..the
+ statements below it are executed as long as one of the conditions on either side is true
+ - So here we check whether c is not equal to blank and if true putchar(executes). If the first condition is true,
+ the second condition which checks for previous character is not even evaluated
+
+ * Once we get a blank, the first condition becomes false and only now is the second condition evaluated. If true, putchar executes
+*/
