b66e43ff7842609b1bce6898cd9b4f9900391035
charlsmaina <mainacharles1496@gmail.com>
2025-10-14 16:04:40 +0300

    counting

--- knr/ch1/charcount2.c
+++ knr/ch1/charcount2.c
@@ -1,10 +1,15 @@
-// using a for loop to count characters: no of digits in a text, no of white spaces, and other characters
+// characters count: no of digits in a text, no of white spaces, and other characters
 /*
 ###############################################################################################################################################################
 #
 # ----------------------------------------code logic ---------------------------------------------------
 # - Any digit in text is internally represented in ascii
 # -It is possible to compare ascii values directly
+# - To detect a digit i use the rg c >= '0' && c <= '9' and to find the numerical value , '0' is minused from c
+# - Everything else is straight forward
+#
+#
+###############################################################################################################################################################
 
 
 
@@ -15,12 +20,34 @@
 #include <stdio.h>
 int main()
 {
-    long int wc;
+    int c, wspace, others;
+    wspace = others = 0;
 
-    for (wc = 0; getchar() != EOF; ++wc)
-        ;
-    if ('1' < '9')
+    int digits[10];
+    for (int i = 0; i < 10; i++)
     {
-        printf("%ld\n", wc);
+        digits[i] = 0;
     }
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            ++digits[c - '0'];
+        }
+        else if (c == '\t' || c == '\n' || c == ' ')
+        {
+            ++wspace;
+        }
+        else
+            ++others;
+    }
+    printf("Digits:\tTally%c", 10); // prints Digits plus a new line:
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\t%d\n", i, digits[i]);
+    }
+    printf("White space:%d\nOthers:%d\n", wspace, others);
+
+    return 0;
 }
