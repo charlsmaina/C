ef4b8f8867301e1c64b080c8435174ed7ec6900d
charlsmaina <mainacharles1496@gmail.com>
2025-10-14 15:26:44 +0300

    you can print a interger value as a character

--- knr/ch1/charcount2.c
+++ knr/ch1/charcount2.c
@@ -1,13 +1,24 @@
-//using a for loop to count characters
+// using a for loop to count characters: no of digits in a text, no of white spaces, and other characters
+/*
+###############################################################################################################################################################
+#
+# ----------------------------------------code logic ---------------------------------------------------
+# - Any digit in text is internally represented in ascii
+# -It is possible to compare ascii values
+
+
+
+
+
+*/
 
 #include <stdio.h>
 int main()
 {
     long int wc;
 
-    for (wc = 0;getchar() != EOF; ++wc)
-    ;
-    printf("%ld\n",wc);
-    
-
+    for (wc = 0; getchar() != EOF; ++wc)
+        ;
+    printf("%ld\n", wc);
+    printf("%i", 10); // prints a new line which is represented by ascii value 10
 }
