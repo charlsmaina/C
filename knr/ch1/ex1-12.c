e2705cc50ad07bb7191bc0b06fa5c0a264e97eac
charlsmaina <mainacharles1496@gmail.com>
2025-10-14 10:26:28 +0300

    logic to cover corner cases

--- knr/ch1/ex1-12.c
+++ knr/ch1/ex1-12.c
@@ -1,19 +1,35 @@
-//Write a program that prints its input one word per line.
+// Write a program that prints its input one word per line.
 
 #include <stdio.h>
 #define IN 1
 #define OUT 0
 
-int main ()
+int main()
 {
     int c;
+    int state;
+    state = OUT;
+
     while ((c = getchar()) != EOF)
     {
-        if (c == ' ' || c == '\t')
-        printf("\n");
-        else
-       putchar(c);
-        
-        
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            if (state == IN)
+            {
+                state = OUT;
+                putchar('\n');
+            }
+        }
+
+        else if (state == OUT) // begiining of a word
+        {
+            state = IN;
+            putchar(c);
+        }
+
+        else if (state == IN) // inside a word
+        {
+            putchar(c);
+        }
     }
 }
