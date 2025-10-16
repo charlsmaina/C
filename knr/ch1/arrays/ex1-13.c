e37dde52791ce00b3e382557f1565305d6cc0c6c
charlsmaina <mainacharles1496@gmail.com>
2025-10-16 08:32:44 +0300

    i used an if staemen t to only print character coints that are greater than 0 but that is abit mechanical since the program still recongonizes word separators as valid words

--- knr/ch1/arrays/ex1-13.c
+++ knr/ch1/arrays/ex1-13.c
@@ -1,17 +1,20 @@
-/*
+/*###
+###########################################################################################################################################3
 Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is
 easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging.
+// This program just counts the number of chracters that a word has:for complete version of the solution check V2 of ex-13.c
+
+---------------------code reedit logic---------------------------------
+ - The problem am facing is that i am not able to distiguish between consequtive word separators
+ -I dont want to print
 */
-
-//This program just counts the number of chracters that a word has:for complete version of the solution check V2 of ex-13.c
-
 
 #include <stdio.h>
 #define IN 1
 #define OUT 0
-int main ()
+int main()
 {
-    int c,charc,state;
+    int c, charc, state;
     charc = 0;
     state = OUT;
 
@@ -19,6 +22,7 @@ int main ()
     {
         if (c == ' ' || c == '\t' || c == '\n')
             state = OUT;
+
         else
             state = IN;
 
@@ -29,10 +33,11 @@ int main ()
         }
         else
         {
-            printf(": %d\n",charc);
-            charc = 0;
+            if (charc > 0)
+            {
+                printf(": %d\n", charc);
+                charc = 0;
+            }
         }
-        
     }
-    
 }
