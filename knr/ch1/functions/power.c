99b006b0e3ec5b207e718a7249042a34fd3dee99
charlsmaina <mainacharles1496@gmail.com>
2025-10-19 08:23:58 +0300

    comment of logic

--- knr/ch1/functions/power.c
+++ knr/ch1/functions/power.c
@@ -1,32 +1,27 @@
-//a program to compute the power  of a given base for a given interger i
+// a program to compute the power  of a given base for a given interger i
 
 #include <stdio.h>
-int power (int base,int n);
+int power(int base, int n);
 
-int main ()
+int main()
 {
     int i;
     printf("i\tPowers of 2:\t Powers of 3\n");
-    for ( i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)
     {
-        printf("%d\t%4d\t\t%5d\n",i,power(2,i),power(3,i)); 
+        printf("%d\t%4d\t\t%5d\n", i, power(2, i), power(3, i));
     }
-    
 
- 
- return 0;
- 
+    return 0;
 }
 
-int power (int base,int n)
+int power(int base, int n)
 {
     int i;
-    int p = 1 ;
-    for ( i = 1; i <= n; i++)
+    int p = 1;
+    for (i = 1; i <= n; i++)
     {
-        p = p * base ;
+        p = p * base; // ie 2 raise 3 = 2 * 2 * 2
     }
     return p;
-    
-
 }
