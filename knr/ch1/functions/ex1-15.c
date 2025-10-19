7a6065a02ce66b2ba880c448b093b8244d4327e1
charlsmaina <mainacharles1496@gmail.com>
2025-10-19 08:52:03 +0300

    using symbolic constants instead of magic numbers , makes the program easy to modify

--- knr/ch1/functions/ex1-15.c
+++ knr/ch1/functions/ex1-15.c
@@ -6,30 +6,32 @@ for conversion.
 #include <stdio.h>
 float deg_fahr(int);
 float fahr_deg(int);
+#define UPPER_LIMIT 300
+#define STEP_SIZE 20
 
-int main ()
+int main()
 
 {
-int i;
-printf("\nFahrenheit to degree conversion:\n\nFahrenheit\tDegrees\n");
-for (i = 0;i <= 100; i = i + 2)
-{
-    printf("%d\t\t%f\n",i,fahr_deg(i));
-}
+    int i;
+    printf("\nFahrenheit to degree conversion:\n\nFahrenheit\tDegrees\n");
+    for (i = 0; i <= UPPER_LIMIT; i += STEP_SIZE)
+    {
+        printf("%2d\t\t%.4f\n", i, fahr_deg(i));
+    }
 
-printf("\nDegree to fahrenheit conversion:\n\nDegreees\tFahrenheit\n");
-for (i = 0;i <= 100; i = i + 2)
-{
-    printf("%d\t\t%f\n",i,deg_fahr(i));
-}
+    printf("\nDegree to fahrenheit conversion:\n\nDegreees\tFahrenheit\n");
+    for (i = 0; i <= UPPER_LIMIT; i += STEP_SIZE)
+    {
+        printf("%d\t\t%.4f\n", i, deg_fahr(i));
+    }
 }
 
 float fahr_deg(int i)
 {
-    return ((5.0/9.0) * (i - 32));
+    return ((5.0 / 9.0) * (i - 32));
 }
 
 float deg_fahr(int i)
 {
-    return ((9.0/5.0) * i + 32);
+    return ((9.0 / 5.0) * i + 32);
 }
