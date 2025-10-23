e80f81862db64b21be8739493172e6badf113109
charlsmaina <mainacharles1496@gmail.com>
2025-10-23 14:48:50 +0300

    pseudocode

---
+++ knr/ch1/ex1-20.c
@@ -0,0 +1,16 @@
+/*
+Exercise 1-20. Write a program detab that replaces tabs in the input with the proper number
+of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns.
+Should n be a variable or a symbolic parameter?
+
+################################################################################################################################################################################
+#
+#---------------------------------------------------------------pseudocode  -----------------------------------------------------------------------------
+# - So the question is about replacing tabs with appropriate blanks spaces:
+# - So i need to read a line and then parse that line
+# - During parsing - when i come across a tab - i will cause a function detab to place the blank spaces instead of placeing the tabs
+#
+# - n should be a symbolic constant since am replacing fixed tabs stop
+###############################################################################################################################################################################
+
+*/
