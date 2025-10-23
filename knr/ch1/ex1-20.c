b27f4614a544597c3f66f47a93fdeceaa0f61fb0
charlsmaina <mainacharles1496@gmail.com>
2025-10-23 15:16:08 +0300

    this working version is not fully consistent. The problem is , it seams to me that tabs at differebt positions are not replaced by constant number of blanks spaces

--- knr/ch1/ex1-20.c
+++ knr/ch1/ex1-20.c
@@ -12,5 +12,69 @@ Should n be a variable or a symbolic parameter?
 #
 # - n should be a symbolic constant since am replacing fixed tabs stop
 ###############################################################################################################################################################################
-
 */
+
+#include <stdio.h>
+#define BLANKS 4
+#define LIMIT 150
+
+int readline(char line[], int lim);
+void detab(void);
+
+int main()
+{
+    int len, i;
+    char line[LIMIT];
+
+    len = 0;
+    while ((len = readline(line, LIMIT)) > 0)
+    {
+        for (i = 0; i < len; i++)
+        {
+            if (line[i] != '\t')
+            {
+                putchar(line[i]);
+            }
+
+            else if (line[i] == '\t')
+            {
+                detab();
+            }
+        }
+    }
+}
+
+int readline(char line[], int lim)
+{
+    int i, j, c;
+    j = 0;
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+    {
+        if (j < lim - 2)
+        {
+            line[j] = c;
+            j++;
+        }
+    }
+
+    if (c == '\n')
+    {
+        line[j] = '\n';
+        j++;
+        i++;
+    }
+    line[j] = '\0';
+
+    return i;
+}
+
+void detab(void)
+{
+    int i;
+    i = 0;
+    while (i < BLANKS)
+    {
+        putchar(' ');
+        i++;
+    }
+}
