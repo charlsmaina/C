67ea23a4c6fce4be6708d8116810809c40de54db
charlsmaina <mainacharles1496@gmail.com>
2025-10-24 07:56:44 +0300

    pseudocode

---
+++ knr/ch1/ex1-21.c
@@ -0,0 +1,40 @@
+/*
+Exercise 1-21. Write a program entab that replaces strings of blanks by the minimum
+number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab.
+When either a tab or a single blank would suffice to reach a tab stop, which should be given
+preference?
+
+##############################################################################################################################################################################3
+#
+# ------------------------------------------------------pseudocode------------------------------------------------------------------------------------
+# - The problem is about replacing blanks by minimum number of tabs and blanks to achieve the same spacing
+# - Previously entab taught me the concept of tab stops.. where the number of spaces to replace the tab space are determined by the number of spaces remaining to reach a tab    #  stop. We do tabwidth - column % tabwidth
+# - it seams , i
+
+
+
+*/
+#include <stdio.h>
+#define TABWIDTH 8
+int main()
+{
+    int position, c, tabc, blanksc;
+    position = 1;
+
+    for (int i = 0; (c = gechar()) != EOF; i++)
+    {
+        if (c == ' ')
+        {
+            if (position % TABWIDTH != 0)
+            {
+                blanksc++;
+                position++;
+            }
+            else
+            {
+                tabc++;
+                blanksc++;
+            }
+        }
+    }
+}
