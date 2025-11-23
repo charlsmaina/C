0199128847bf55dca86f99b9f9f12301d2651157
charlsmaina <mainacharles1496@gmail.com>
2025-11-23 18:08:48 +0300

    chars

--- knr/ch2/dum.c
+++ knr/ch2/dum.c
@@ -5,40 +5,35 @@
 
 int z;
 
-
 int main()
 {
     char x = 'x';
     int y;
-    printf("%d\n",y);//uninitialized automatic variables hold dummy values
- 
-   printf("%d\n",x);// a character onstant can be interpreted as both a character and an interger
+    printf("%d\n", y); // uninitialized automatic variables hold dummy values
 
-   printf("%c\n",x);
+    printf("%d\n", x); // a character onstant can be interpreted as both a character and an interger
 
-   printf("%d\n",z);//uninitialized global/external variables hold a value of zero
+    printf("%c\n", x);
 
-   const char msg[7] = "warning";
-   printf("%s\n",msg);
+    printf("%d\n", z); // uninitialized global/external variables hold a value of zero
 
+    const char msg[7] = "warning";
+    printf("%s\n", msg);
 
     int c = 'R';
-   if (c >= 'A' && c <= 'Z')// converts a higher case letter to lower case.
-   {
-    char lower = (c + 'a' - 'A');
+    if (c >= 'A' && c <= 'Z') // converts a higher case letter to lower case.
+    {
+        char lower = (c + 'a' - 'A');
 
-    printf("%c\n",lower);
-   }
+        printf("%c\n", lower);
+    }
 
-   else
-        printf("%c\n",c);
+    else
+        printf("%c\n", c);
 
-    printf("%d\n",'a');
-    printf("%d\n,",'A');
-    printf("%d\n",'B' - 'b');
+    printf("%d\n", 'a');
+    printf("%d\n,", 'A');
+    printf("%d\n", 'B' - 'b');
 
     return 0;
-   
 }
-
-
