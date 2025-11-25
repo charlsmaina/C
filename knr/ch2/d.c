e3ab3acdc5613c92caca76249fe2b5eee6ef373a
charlsmaina <mainacharles1496@gmail.com>
2025-11-25 08:04:34 +0300

    unsigned or signed char

--- knr/ch2/d.c
+++ knr/ch2/d.c
@@ -8,5 +8,10 @@ int main()
     char c = 'c';
     printf("This is letter %c\n", c);                    // prints c as a character constnt
     printf("%c is represented internlly as %d\n", c, c); // prints c both as a character and the nteger value it represents
+
+    //----when char is converted to an nteger can it ever produce a negative number---lets find out//
+    int z = 100;
+    printf("%c\n", z);
+    // It seams like there are some unprintable characters tht re represented by question mark like symbol.
     return 0;
 }
