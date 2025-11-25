961f31df7ec886348e6e9d38fc6bae73a882bafd
charlsmaina <mainacharles1496@gmail.com>
2025-11-25 08:04:59 +0300

    n\a

--- knr/ch2/ex2-2.c
+++ knr/ch2/ex2-2.c
@@ -1,34 +1,27 @@
 
-//Write a loop equivalent to the for loop above without using && or ||.
-//Here is the supposed for loop:
+// Write a loop equivalent to the for loop above without using && or ||.
+// Here is the supposed for loop:
 /*
     for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
         s[i] = c;
 */
 
-
-
-
 #include <stdio.h>
 int main()
 {
     int lim = 100;
-    char s [100];
+    char s[100];
     int i = 0;
-   while (i < lim-1)
-   {
-    int c;
-    if (c = getchar() != '\n')
+    while (i < lim - 1)
     {
-        if (c != EOF)
+        int c;
+        if (c = getchar() != '\n')
         {
-            s[i] = c;
-            i++;
+            if (c != EOF)
+            {
+                s[i] = c;
+                i++;
+            }
         }
-        
     }
-    
-   }
-   
-  
 }
