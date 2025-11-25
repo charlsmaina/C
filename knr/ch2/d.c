897b123a9bbc40b34b8684f682b26ec344e7c166
charlsmaina <mainacharles1496@gmail.com>
2025-11-25 06:56:02 +0300

    character constands and character strin distinction

--- knr/ch2/d.c
+++ knr/ch2/d.c
@@ -3,5 +3,10 @@ int main()
 {
     char pattern[] = "Hello";
     printf("%s\n", pattern);
+
+    //----------------------//
+    char c = 'c';
+    printf("This is letter %c\n", c);                    // prints c as a character constnt
+    printf("%c is represented internlly as %d\n", c, c); // prints c both as a character and the nteger value it represents
     return 0;
 }
