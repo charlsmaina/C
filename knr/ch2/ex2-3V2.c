c9a08ad291383ef5e38662c24083e88b9b2927f0
charlsmaina <mainacharles1496@gmail.com>
2025-11-27 12:24:21 +0300

    incrementing hexadecimal

--- knr/ch2/ex2-3V2.c
+++ knr/ch2/ex2-3V2.c
@@ -23,6 +23,7 @@
 */
 
 #include <stdio.h>
+
 #define LIMIT 6
 
 int _raise_16(int n);
@@ -33,38 +34,38 @@ void increment(char hex[]);
 int main()
 {
     char hex[LIMIT] = {'0', 'x', '0', '0', '0', '0'};
-    increment(hex);
+
+    for (int x = 0; x <= 256; x++)
+    {
+        printf("Decimal:  %d  Hexadecimal: %s\n", x, hex);
+        increment(hex);
+    }
 }
 
 void increment(char hex[])
 {
     for (int i = 5; i >= 2; i--)
     {
-
-        int j;
-        for (j = 0; j <= 9; j++)
+        if (hex[i] >= '0' && hex[i] <= '8')
         {
-
-            printf("Hexadecimal: %s\n", hex);
-
             hex[i]++;
+            break;
         }
-        while (j > 9 && j <= 15)
-        {
-            if (j == 10)
-            {
-                hex[i] = 'A';
-                printf("Hexadecimal: %s\n", hex);
-                j++;
-            }
-            else
-            {
 
-                hex[i]++;
-                printf("Hexadecimal: %s\n", hex);
-                j++;
-            }
+        else if (hex[i] == '9')
+        {
+            hex[i] = 'A';
+            break;
         }
-        hex[i] = '0';
+
+        else if (hex[i] >= 'A' && hex[i] <= 'E')
+        {
+            hex[i]++;
+            break;
+        }
+        else if (hex[i] == 'F')
+        {
+            hex[i] = '0';
+        }
     }
 }
