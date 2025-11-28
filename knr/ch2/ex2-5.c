90a6245109424924cf83b95d9899f270e21556ca
charlsmaina <mainacharles1496@gmail.com>
2025-11-28 14:00:20 +0300

    characters tracking

--- knr/ch2/ex2-5.c
+++ knr/ch2/ex2-5.c
@@ -3,4 +3,67 @@ Exercise 2-5. Write the function any(s1,s2), which returns the first location in
 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
 (The standard library function strpbrk does the same job but returns a pointer to the
 location.)
+
+#################################################################################################################################################################################
+#
+#-------------------------pseudocode ---------------------------------
+#   -So this function any is supposed to check for the location in s1 where a character in s2 occurs
+#   - So parse s2 characterwise - then parse s1 stringwise - incase i find a match - return the position:
+#
+#
 */
+#include <stdio.h>
+#define LIMIT 20
+int ge_tline(char line[], int lim);
+int any(char s1[], char s2[]);
+
+int main()
+{
+    char s1[LIMIT];
+    char s2[LIMIT];
+
+    int len1, len2, pos;
+    while ((len1 = ge_tline(s1, LIMIT)) > 0 && (len2 = ge_tline(s2, LIMIT)) > 0)
+    {
+
+        for (int i = 0; s2[i] != '\0'; i++)
+        {
+            char temp[2] = {s2[i], '\0'};
+            pos = any(s1, temp);
+
+            if (pos == -1)
+            {
+                printf("%c does not occur in s1\n", s2[i]);
+            }
+            else
+                printf("%c in s2 occurs first in index %d   in s1\n", s2[i], pos);
+        }
+    }
+}
+int ge_tline(char line[], int lim)
+{
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    {
+        line[i] = c;
+    }
+    if (c == '\n')
+    {
+        line[i] = '\0';
+    }
+
+    return i;
+}
+int any(char s1[], char s2[])
+{
+    int i, j;
+    for (i = 0; s2[i] != '\0'; i++)
+    {
+        for (j = 0; s1[j] != s2[i] && s1[j] != '\0'; j++)
+            ;
+        if (s1[j] == '\0')
+            return -1;
+        else
+            return j;
+    }
+}
