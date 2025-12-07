6a42c4310a168c6e45c0f9638156ae157adfc2b8
charlsmaina <mainacharles1496@gmail.com>
2025-12-07 06:33:39 +0300

    review ones's and 2's Complements

---
+++ knr/ch2/ex2-9.c
@@ -0,0 +1,6 @@
+/*Exercise 2-9. In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit
+in x. Explain why. Use this observation to write a faster version of bitcount.
+--- i need to review number system - what does 1 complement and two complement look like -----the expression x&= (x-1) makes sense but i need a deeper understanding of the 1 and 2 complements
+
+
+*/
