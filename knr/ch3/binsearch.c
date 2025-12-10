c1f7c236a1615988521c68f4aef05ab9b7a74209
charlsmaina <mainacharles1496@gmail.com>
2025-12-10 08:43:06 +0300

    low and high update changed to middle : and not to middle + 1

--- knr/ch3/binsearch.c
+++ knr/ch3/binsearch.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
+#define LIMIT 100
 
-int binary_search(int digits[]);
+int binary_search(int, int digits[], int);
 int main()
 {
-    int digits[100];
-    for (int i = 0; i < 100; i++)
+    int digits[LIMIT];
+    int found, number;
+    number = 51;
+    printf("Break 1\n");
+    for (int i = 0; i < LIMIT; i++)
     {
+
         digits[i] = i;
     }
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < LIMIT; i++)
     {
-        printf("%6d%c", digits[i], (digits[i] % 10 == 9 || i == 99) ? '\n' : ' ');
+        printf("%3d%c", digits[i], (digits[i] % 10 == 9 || i == 99) ? '\n' : ' ');
     }
+
+    printf("Break 2\n");
+    found = binary_search(number, digits, LIMIT);
+    printf("Found:%d\n", found);
+}
+int binary_search(int number, int digits[], int lim)
+{
+
+    int low, high, middle;
+    low = 0;
+    high = lim - 1;
+    while (low <= high)
+    {
+        middle = (low + high) / 2;
+        printf("Middle index: %d\n", middle);
+        printf("Middle digit:%d\n", digits[middle]);
+
+        if (number > digits[middle])
+        {
+            low = middle;
+            printf("break 3");
+        }
+        else if (number < digits[middle])
+        {
+            high = middle;
+        }
+        else
+            return middle;
+    }
+
+    return -1;
 }
