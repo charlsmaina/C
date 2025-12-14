381e5fba0d1b66b94feec187dd0e25303c5aad15
charlsmaina <mainacharles1496@gmail.com>
2025-12-14 07:21:50 +0300

    reverse.c function defination

---
+++ knr/lib/reverse.c
@@ -0,0 +1,22 @@
+#include "reverse.h"
+#include <stdio.h>
+
+void reverse(char s[])
+{
+    printf("String:%s", s);
+    int i, j;
+    char temp;
+    i = j = 0;
+    while (s[j] != '\0')
+    {
+        j++;
+    }
+    for (; i < j; i++)
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[i] = temp;
+        j--;
+    }
+    printf("\nReversed string:%s", s);
+}
