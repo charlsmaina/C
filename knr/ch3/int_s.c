1187aea6365a81bdbf7c0cf7620a4d30a8e6b366
charlsmaina <mainacharles1496@gmail.com>
2025-12-14 07:50:46 +0300

    string to int conversion and then int to string conversion

---
+++ knr/ch3/int_s.c
@@ -0,0 +1,16 @@
+#include "../headers/itoa.h"
+#include "../headers/reverse.h"
+#include "../headers/ge_tline.h"
+#include <stdio.h>
+#define LIMIT 10
+
+int main(void)
+{
+    int len, n;
+    char s[LIMIT];
+    while ((len = ge_tline(s, LIMIT)) > 0)
+    {
+        n = atoi(s);
+        itoa(n, s);
+    }
+}
