df0043a3fb42c9d38daf369054556efdfbd60322
charlsmaina <mainacharles1496@gmail.com>
2025-12-15 09:17:41 +0300

    using string.h libraly

--- knr/lib/strindex.c
+++ knr/lib/strindex.c
@@ -1,21 +1,19 @@
 #include "../headers/strindex.h"
 #include <stdio.h>
+#include <string.h>
 int strindex(char s[], char t[])
 {
     int i, j, k;
     i = j = 0;
 
     int len_t;
-    while (s[i++] != '\0')
-        ;
-
-    while (t[j++] != '\0')
-        ;
-    j--;
-    len_t = j;
-    for (; i >= 0; i--)
+    i = strlen(s);
+    printf("Len:%d\n", i);
+    len_t = strlen(t);
+    printf("Len pattern:%d\n", len_t);
+    for (i = i - 1; i >= 0; i--)
     {
-        for (k = i, j = len_t; s[k] == t[j] && j >= 0; k--, j--)
+        for (k = i, j = (len_t - 2); s[k] == t[j] && j >= 0; k--, j--)
             ;
         if (j <= 0)
         {
