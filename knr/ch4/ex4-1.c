bbc05ab80d49dad08e7083789fd82d90c73bf4b8
charlsmaina <mainacharles1496@gmail.com>
2025-12-15 08:46:17 +0300

    working version

--- knr/ch4/ex4-1.c
+++ knr/ch4/ex4-1.c
@@ -8,8 +8,25 @@ occurrence of t in s, or -1 if there is none.
 #   - The other option - transverse from right to left
 #   - Thats what seams obvious to me
 #   - I need a function strindex(s,t)
-#
-#
-#
-
+#   -And it worked!!
 */
+#include "../headers/strindex.h"
+#include "../headers/ge_tline.h"
+#include <stdio.h>
+#define LIMIT 100
+#define PATERN_LEN 10
+int main(void)
+{
+    char line[LIMIT];
+    char pattern[PATERN_LEN];
+    printf("Enter lines to scan for a certain pattern:\n");
+    while (ge_tline(line, LIMIT) > 0)
+    {
+        printf("Enter the pattern to find:\n");
+        if (ge_tline(pattern, PATERN_LEN) > 0)
+        {
+            printf("Pattern occurs in rightmost position at index:%d\n", strindex(line, pattern));
+        }
+    }
+    return 0;
+}
