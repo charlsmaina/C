e4a8d9a65f3611a1193e2637d03a777251df5ca7
charlsmaina <mainacharles1496@gmail.com>
2025-12-15 07:50:21 +0300

    ^T^Z

---
+++ knr/ch4/ex4-1.c
@@ -0,0 +1,15 @@
+/*
+Exercise 4-1. Write the function strindex(s,t) which returns the position of the rightmost
+occurrence of t in s, or -1 if there is none.
+#############################################################################################################################################
+#
+#-----------------------------pseudocode-------------------------------------------------
+#   - Rigthmost occurence means i need to have a mechanism to ignore left occurences - array trnasversal is mainly from left to right
+#   - The other option - transverse from right to left
+#   - Thats what seams obvious to me
+#   - I need a function strindex(s,t)
+#
+#
+#
+
+*/
