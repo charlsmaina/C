0ab9c312b19976852b40a8219dc71391137fdf2a
charlsmaina <mainacharles1496@gmail.com>
2026-02-27 15:10:11 +0300

    modified the function to allow for user to specify the type of pattern to check

---
+++ knr/ch4/patt_check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../headers/ge_tline.h"
+#include "../headers/strindex.h"
+#define MAXLINE 100
+
+int main()
+{
+    char line[MAXLINE];
+    char pattern[] = "ould";
+
+    int len;
+}
--- knr/lib/strindex.c
+++ knr/lib/strindex.c
@@ -1,24 +1,32 @@
 #include "../headers/strindex.h"
 #include <stdio.h>
 #include <string.h>
-int strindex(char s[], char t[])
+#include "../headers/ge_tline.h"
+int strindex(char s[], char t[], int size_t)
 {
-    int i, j, k;
-    i = j = 0;
+    printf("Enter pattern to check:\n\n");
+    int len = 0;
 
-    int len_t;
-    i = strlen(s);
-    printf("Len:%d\n", i);
-    len_t = strlen(t);
-    printf("Len pattern:%d\n", len_t);
-    for (i = i - 1; i >= 0; i--)
+    while ((len = ge_tline(t, size_t)) > 0)
     {
-        for (k = i, j = (len_t - 2); s[k] == t[j] && j >= 0; k--, j--)
-            ;
-        if (j <= 0)
+        int i, j, k;
+        int len_t;
+        i = strlen(s);     // length of line
+        len_t = strlen(t); // length of pattern
+
+        printf("Len line:%d\n", i);
+        printf("Len pattern:%d\n", len_t);
+
+        for (i = i - 1; i >= 0; i--)
         {
-            return k + 1;
+            for (k = i, j = (len_t - 2); s[k] == t[j] && j >= 0; k--, j--)
+                ;
+            if (j <= 0)
+            {
+                return k + 1;
+            }
         }
     }
+
     return -1;
 }
