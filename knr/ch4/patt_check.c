94fc1fd3649bcc67d69c4e1020123d571167fef3
charlsmaina <mainacharles1496@gmail.com>
2026-02-27 15:12:03 +0300

    a program to check a pattern in a line and print that line if a line is found

--- knr/ch4/patt_check.c
+++ knr/ch4/patt_check.c
@@ -2,11 +2,22 @@
 #include "../headers/ge_tline.h"
 #include "../headers/strindex.h"
 #define MAXLINE 100
+#define PATTERN_LEN 10
 
 int main()
 {
     char line[MAXLINE];
-    char pattern[] = "ould";
+
+    char pattern[PATTERN_LEN];
 
     int len;
+    len = 0;
+
+    while ((len = ge_tline(line, MAXLINE)) > 0)
+    {
+        if (strindex(line, pattern, PATTERN_LEN) >= 0)
+        {
+            printf("%s", line);
+        }
+    }
 }
