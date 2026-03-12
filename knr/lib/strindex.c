6b9c3933da70ff3cc6be12562765cdccdc040d56
charlsmaina <mainacharles1496@gmail.com>
2026-03-12 17:14:59 +0300

    clean end of line check

--- knr/lib/strindex.c
+++ knr/lib/strindex.c
@@ -10,10 +10,9 @@ int strindex(char line[], char pattern[])
     int len_line;
     int len_pattern;
 
-    len_line = strlen(line);       // length of line
     len_pattern = strlen(pattern); // length of pattern
 
-    for (i = 0; i < len_line - 1; i++)
+    for (i = 0; i < line[i] != '\0'; i++)
     {
         for (k = i, j = 0; j < len_pattern - 1 && line[k] == pattern[j]; k++, j++) /*len_pattern-1 because of the nature of the working of the ge_tline: so essentially ge_tline appends a newline and a null terminator to every string that is handled to it. strlen outputs the newline as part of that length but we dont want the newline to be part of the pattern: so we check the pattern up to len_pattern - 1: so essentially the element at pattern[len_pattern - 1] is a new line character*/
             ;
