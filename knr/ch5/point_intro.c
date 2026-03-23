414ade3ad2626b36feeb79662eb7824d8074724d
charlsmaina <mainacharles1496@gmail.com>
2026-03-23 19:24:02 +0300

    pointer type must match the type it points to

--- knr/ch5/point_intro.c
+++ knr/ch5/point_intro.c
@@ -7,10 +7,17 @@ int main()
     int y = 2;
     char name[] = "Charles";
 
-    int *ip;
-    ip = &x;
+    int *ip; // pointer declaration
+    ip = &x; // use of indirection and dereferncing + address assignment operators
     printf("Pointer pointing to x prints:%d\n", *ip);
 
-    *ip = 10; // updates the new value of x
+    *ip = 10; // updates the new value of x// updating the pointer updates the value of the variable it points to
     printf("New value of x is :%d\n", x);
+
+    y = *ip; // assigns value of x to y
+    printf("New value of y is :%d\n", y);
+
+    char *ip2; // pointer type must match the type of data it stores
+    ip2 = &name[0];
+    printf("Pointer pointing to name prints:%d\n", *ip2);
 }
