e6defdc7ce24210ba5f69deb914ad023235892b5
charlsmaina <mainacharles1496@gmail.com>
2026-03-24 07:57:41 +0300

    getint using a pointer

--- knr/ch5/get_int.c
+++ knr/ch5/get_int.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int get_int(int *pn);
 int getch(void);
-void ungetch();
+void ungetch(int);
+#define SIZE 100
 
 int main()
 {
+    int numbers[SIZE];
+    for (int i = 0; i < SIZE && get_int(&numbers[i]); i++) // populates an array with numbers
+        ;
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Interger value at index %d is :%d\n", i, numbers[i]);
+    }
+}
+
+int get_int(int *pn) // gets a number and stores in in the pointer: int return is for EOF - signaling number
+{
+    int c;
+    int sign;
+    while (isspace(c = getch())) // skip whitespace
+        ;
+    if (!isdigit(c) && c != EOF && c != '-' && c != '+')
+    {
+        ungetch(c); /*Not a number*/
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (sign == '-' || sign == '+')
+    {
+        c = getch();
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+    {
+        *pn = 10 * *pn + (c - '0');
+    }
+    *pn *= sign;
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return c;
+}
+
+int BUFFER[SIZE];
+int buf_pointer = 0;
+
+int getch(void)
+{
+    return buf_pointer > 0 ? BUFFER[--buf_pointer] : getchar();
+}
+void ungetch(int c)
+{
+    if (buf_pointer < SIZE)
+    {
+        BUFFER[buf_pointer++] = c;
+    }
+    else
+        printf("Error: Buffer full\n");
 }
