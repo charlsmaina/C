48c0a9fabaa550d005ff6ccdcca04f46d343b18c
charlsmaina <mainacharles1496@gmail.com>
2026-03-25 06:17:28 +0300

    exercise 5 pseudocode

---
+++ knr/ch5/ex5-1.c
@@ -0,0 +1,7 @@
+/*
+Exercise 5-1. As written, getint treats a + or - not followed by a digit as a valid
+representation of zero. Fix it to push such a character back on the input.
+
+--------------------------------problem statement------------------------------------------------
+# + and - are treated as a valid representation of zero. Is this zero the one we are returning for non-numbers inputs?. If it is the one. So am to fix that so that such a character is pushed back on the input. Am not getting the question.
+*/
