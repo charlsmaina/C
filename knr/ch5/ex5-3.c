0b2e24aac95355973b34d03d16310113e25aa64b
charlsmaina <mainacharles1496@gmail.com>
2026-03-27 07:45:54 +0300

    string concatenation using pointers

--- knr/ch5/ex5-3.c
+++ knr/ch5/ex5-3.c
@@ -13,32 +13,43 @@ strcat(s,t) copies the string t to the end of s.
 */
 
 #include <stdio.h>
-#define BUFFER 150
+#define BUFFER 15
 
-char *my_strcat(char *s, char *t);
-static char copying_buffer[BUFFER];
-static char *buf_p = copying_buffer;
-
-char *alloc(int n);
+char *my_strcat(char *s, char *t);   /*this is a function that returns a char pointer to the starting address of the beginning of the copied string*/
+static char copying_buffer[BUFFER];  /*This is the borrowed memory that we use to concantenate the two strings together*/
+static char *buf_p = copying_buffer; /*This is the buffer manager: tracks memory to make sure there is enough to hold the combined two strings*/
+/*buf_p is global because it is to be shared by many function calls..it has to be consistent*/
+char *alloc(int n); /*This our memory allocate: n is the total length of the concantenated string*/
 
 int main()
 {
-    char *rp;
+    char *rp; // used to store the address where our concat string starts
     char *name = "Charles";
     char *sur_name = "Kinyua";
+
+    char *college = "JKUAT";
+    char *uni = "University";
+
     rp = my_strcat(name, sur_name);
     while (*rp != '\0')
     {
         putchar(*rp++);
     }
     putchar('\n');
+    char *rp2;
+    rp2 = my_strcat(college, uni);
+    while (*rp2 != '\0')
+    {
+        putchar(*rp2++);
+    }
+    putchar('\n');
 }
 
 char *my_strcat(char *s, char *t)
 {
-    int s_len;
+    int s_len; /*for counting the length of s*/
     int t_len;
-    char *start;
+    char *start; /*stores the beginning of our allocated memory*/
     char *temp_s;
     char *temp_t;
 
@@ -61,12 +72,12 @@ char *my_strcat(char *s, char *t)
         t_len++;
     }
 
-    alloc_p = alloc(s_len + t_len + 1);
-    start = alloc_p;
+    alloc_p = alloc(s_len + t_len + 1); /*buffer memory llocation: +1 to accomodate null terminator in the string*/
+    start = alloc_p;                    // to store the beginning of the string in the buffer
 
     while ((*alloc_p++ = *s++) != '\0')
         ;
-    alloc_p--;
+    alloc_p--; /* overides null terminator for the s string*/
     while ((*alloc_p++ = *t++) != '\0')
         ;
     return start;
@@ -74,7 +85,7 @@ char *my_strcat(char *s, char *t)
 
 char *alloc(int n)
 {
-    if ((copying_buffer + BUFFER - buf_p) >= n)
+    if ((copying_buffer + BUFFER - buf_p) >= n) /*checks if there is sufficient space before it allocates memory*/
     {
         buf_p += n;
         return buf_p - n;
