9ddaa9cb838d197629f8c9f76df163a79f6497ca
charlsmaina <mainacharles1496@gmail.com>
2026-04-04 22:15:20 +0300

    pointers

--- knr/ch5/ex5-4v2.c
+++ knr/ch5/ex5-4v2.c
@@ -5,7 +5,8 @@ end of the string s, and zero otherwise.
 -----------------------problem statement---------------------------
 #   check if a string ends with a given string
 -----------------------pseudocode----------------------------------------------
-# Use two nested loop : the two strings should be pointers: for each character in string 1 check if there is a match in string 2 and return a 1 if null is reached in string 2
+# In the previous exercise , the  way computation is done is somehow so mechanical: we stop at each character and examine the end string multiple times
+# In this version: ending string is checked starting from the end of the string then backwards. So , eventually we do just a single check
 #
 */
 
@@ -47,20 +48,25 @@ int main()
 
 int strend(char *string_1, char *string_2)
 {
-    char *temp;
-    char *temp_2;
+    int len_string_2 = 0;
 
-    while (*string_1)
+    while (*string_2++) // when this loop is false, string_2 points to the null terminator
+        len_string_2++; // by the time we exit, len_string has  not counted null
+    while (*string_1++) // loop exits when string_1 points to null
+        ;
+    // since the point to one character more tha
+    string_2--;
+    string_1--;
+    while (*string_1 == *string_2 && len_string_2 > 0)
     {
-        temp = string_1;
-        temp_2 = string_2;
-
-        while (*temp_2++ == *temp && *temp++)
-            ;
-        string_1++;
-
-        if (*temp == '\0' && *temp_2 == '\0')
-            return 1;
+        string_1--;
+        string_2--;
+        len_string_2--;
     }
-    return 0;
+    if (len_string_2 == 0)
+    {
+        return 1;
+    }
+    else
+        return 0;
 }
