b74235012a7e258a8567aa2040644f3b7af65347
charlsmaina <mainacharles1496@gmail.com>
2026-04-27 18:35:03 +0300

    pointer and arrays interoperability..ie, it is possible to index pointers like arrays

--- knr/ch5/point_trials.c
+++ knr/ch5/point_trials.c
@@ -2,9 +2,26 @@
 #include <stddef.h>
 int main()
 {
-    char *name = "CHARLES";
+    char *name = "CHARLES ";
     char *sur_name = "MAINA";
 
-    name = sur_name;
+    char nick_name[] = "CHARO";
+
     printf("First letter for surname:%c\n", *name);
+    printf("Second letter of surname:%c\n", name[1]); /*You can index a pointer*/
+    printf("Hello, \n");
+    while (*name != ' ')
+    {
+        putchar(*name);
+        name++;
+    }
+    putchar('\n');
+    printf("Niajy ");
+    char *nick_name_ptr = nick_name;
+    while (*nick_name_ptr != '\0')
+    {
+        putchar(*nick_name_ptr);
+        nick_name_ptr++;
+    }
+    putchar('\n');
 }
