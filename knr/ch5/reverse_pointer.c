a1db140360b977811c50e12909f86192bba25868
charlsmaina <mainacharles1496@gmail.com>
2026-04-28 11:40:35 +0300

    a pointer has to point to something for it to be useful

---
+++ knr/ch5/reverse_pointer.c
@@ -0,0 +1,37 @@
+/* pointer version of reverse string*/
+#include <stdio.h>
+#include <stddef.h>
+void reverse(char *string_pointer);
+int main()
+{
+    char name[] = "MAINA";
+    reverse(name);
+    char *str_ptr = name;
+    while (*str_ptr)
+    {
+        putchar(*str_ptr++);
+    }
+    putchar('\n');
+}
+void reverse(char *string_pointer)
+{
+    char *temp_string_pointer = string_pointer;
+
+    while (*temp_string_pointer)
+    {
+        temp_string_pointer++;
+    }
+    temp_string_pointer--;
+
+    while (temp_string_pointer > string_pointer)
+    {
+        int temp;
+        temp = *string_pointer;
+
+        *string_pointer = *temp_string_pointer;
+        *temp_string_pointer = temp;
+
+        temp_string_pointer--;
+        string_pointer++;
+    }
+}
