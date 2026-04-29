4022e06f2af1bf49c01ee5ce74c73b81035a9e8a
charlsmaina <mainacharles1496@gmail.com>
2026-04-29 19:27:42 +0300

    structs basics

--- knr/ch6/structs_basics.c
+++ knr/ch6/structs_basics.c
@@ -13,4 +13,23 @@ int main()
     pt.y = 21;
 
     printf("X cordinate: %d\nY coordinate: %d\n", pt.x, pt.y);
+
+    struct rect
+    {
+        struct
+        {
+            int x;
+            int y;
+        } p1;
+        struct
+        {
+            int x;
+            int y;
+        } p2;
+    };
+    struct rect screen;
+    screen.p1.x = 14;
+    screen.p1.y = 25;
+
+    printf("Top right cordinates of screen\nX coordinate: %d\nY coordinate: %d\n", screen.p1.x, screen.p1.y);
 }
